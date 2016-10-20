74c251252c87fec1d97629503a9a7d64c0b84cfa
EarvinKayonga <demoneartin@gmail.com>
2016-10-20 15:57:23 +0200

    phantomjs

--- Dockerfile
+++ Dockerfile
@@ -1,58 +1,22 @@
-FROM mhart/alpine-node:6.3
+FROM java:8
 MAINTAINER Earvin Kayonga <e.kayonga@bevolta.com>
 
 ENV EMBER_CLI_VERSION 2.4.1
 ENV BOWER_VERSION 1.7.1
-ENV PHANTOMJS_VERSION 1.9.19
+ENV PHANTOMJS_VERSION 1.9.7
 ENV WATCHMAN_VERSION 3.5.0
-
+ENV SCALA_VERSION 2.11.8
+ENV SBT_VERSION 0.13.12
 
 ENV LANG en_US.utf8
 
-# Install ruby & ruby-dev
-RUN apk add  --update bash ruby ruby-dev git python
+ADD install.sh .
+RUN chmod +x install.sh && ./install.sh
 
-# Install sass and compass gems
-# install compass --no-ri --no-rdoc --pre sass-css-importer
-RUN \
-apk add --update \
-    ruby \
-    libffi-dev \
-    build-base
-RUN gem sources --add https://rubygems.org/
-RUN gem update --no-rdoc --no-ri
-RUN gem install \
-		compass --no-ri --no-rdoc \
-    listen \
-		ffi \
-		--pre sass-css-importer \
-    compass &&\
+# Set up user jenkins
+RUN useradd -m -s /bin/bash jenkins
+RUN echo jenkins:jenkins | chpasswd
+RUN mkdir -p /var/run/sshd
 
-rm -rf /var/cache/apk/*
-
-RUN \
-apk --update add git build-base &&\
-git clone https://github.com/sass/sassc &&\
-cd sassc &&\
-git clone https://github.com/sass/libsass &&\
-SASS_LIBSASS_PATH=/sassc/libsass make &&\
-
-# install
-mv bin/sassc /usr/bin/sass &&\
-
-# cleanup
-cd / &&\
-rm -rf /sassc &&\
-apk add libstdc++
-
-
-# ember-cli, bower and phantomjs
-RUN npm install -g ember-cli@$EMBER_CLI_VERSION
-RUN npm install -g bower@$BOWER_VERSION
-RUN npm install -g node-sass
-#RUN npm install -g phantomjs@$PHANTOMJS_VERSION
-
-# run ember server on container start
-EXPOSE 4200 49152
-ENTRYPOINT ["/usr/bin/ember"]
-CMD ["serve","--live-reload=false"]
+EXPOSE 22
+CMD /usr/sbin/sshd -D
---
+++ install.sh
@@ -0,0 +1,50 @@
+echo "installing ember and its dependencies"
+# ember-cli, bower and phantomjs
+
+apt-get -y update && apt-get -y install  g++ build-essential npm nodejs-legacy;
+npm install -g ember-cli@$EMBER_CLI_VERSION
+npm install -g bower@$BOWER_VERSION
+npm install -g node-sass
+
+# phantomjs
+apt-get update && \
+  apt-get upgrade -y && \
+  apt-get install -y vim git wget libfreetype6 libfontconfig bzip2 && \
+  mkdir -p /srv/var && \
+  wget -q --no-check-certificate -O /tmp/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 && \
+  tar -xjf /tmp/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 -C /tmp && \
+  rm -f /tmp/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2 && \
+  mv /tmp/phantomjs-$PHANTOMJS_VERSION-linux-x86_64/ /srv/var/phantomjs && \
+  ln -s /srv/var/phantomjs/bin/phantomjs /usr/bin/phantomjs && \
+  git clone https://github.com/n1k0/casperjs.git /srv/var/casperjs && \
+  ln -s /srv/var/casperjs/bin/casperjs /usr/bin/casperjs && \
+  apt-get autoremove -y && \
+  apt-get clean all;
+
+echo "installing ruby"
+# Install ruby & ruby-dev
+ apt-get -y update && apt-get -y install  ruby ruby-dev git python;
+
+ gem sources --add https://rubygems.org/
+ gem update --no-rdoc --no-ri
+ gem install \
+		compass --no-ri --no-rdoc \
+		ffi \
+		--pre sass-css-importer \
+    compass;
+
+
+# Install Scala
+  echo "installing Scala"
+## Piping curl directly in tar
+  curl -fsL http://downloads.typesafe.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.tgz | tar xfz - -C /root/ && \
+  echo >> /root/.bashrc && \
+  echo 'export PATH=~/scala-$SCALA_VERSION/bin:$PATH' >> /root/.bashrc;
+
+# Install sbt
+  curl -L -o sbt-$SBT_VERSION.deb http://dl.bintray.com/sbt/debian/sbt-$SBT_VERSION.deb && \
+  dpkg -i sbt-$SBT_VERSION.deb && \
+  rm sbt-$SBT_VERSION.deb && \
+  apt-get update && \
+  apt-get install sbt && \
+  sbt sbtVersion;
